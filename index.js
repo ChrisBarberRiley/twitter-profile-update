8653c7a5ab6ef47ec499450240872ef955057b44
Chris Barber-Riley <chris.barberriley@gmail.com>
2021-05-29 23:26:39 +0100

    Errors with header size

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import T from './config.js';
+
 // T.get(
 //   'search/tweets',
 //   { q: 'banana since:2011-07-11', count: 10 },
@@ -6,35 +9,65 @@
 //   },
 // );
 
-// T.post(
-//   'statuses/update',
-//   { status: 'hello world!' },
+// Set profile image
+// imageToBase64('./profile_img.jpg') // Image URL
+//   .then((image) => {
+//     console.log(image); // "iVBORw0KGgoAAAANSwCAIA..."
+
+//     T.post(
+//       'account/update_profile_image',
+//       { image },
+//       function (err, data, response) {
+//         console.log(data);
+//       },
+//     );
+//   })
+//   .catch((error) => {
+//     console.log(error); // Logs an error if there was one
+//   });
+
+// // Set banner image
+// imageToBase64('./test.jpg') // Image URL
+//   .then((image) => {
+//     // console.log(image); // "iVBORw0KGgoAAAANSwCAIA..."
+
+//     T.post(
+//       'account/update_profile_banner',
+//       { banner: image },
+//       function (err, data, response) {
+//         console.log(data);
+//         console.log('response', response);
+//         console.log('err', err);
+//       },
+//     );
+//   })
+//   .catch((error) => {
+//     console.log(error); // Logs an error if there was one
+//   });
+
+const changeBanner = async () => {
+  console.log('Updating banner!');
+  try {
+    const images = fs.readdirSync('./images');
+    const img = fs.readFileSync('./images/' + images[0]).toString('base64');
+    await T.post(
+      'account/update_profile_banner',
+      { image: img },
+      (err, data, response) => {
+        console.log('data', data);
+      },
+    );
+  } catch (err) {
+    console.log('Update failed:', err);
+  }
+};
+
+changeBanner();
+
+// T.get(
+//   'users/profile_banner',
+//   { user_id: '331379561' },
 //   function (err, data, response) {
 //     console.log(data);
 //   },
 // );
-
-function toDataURL(src, callback, outputFormat) {
-  var img = new Image();
-  img.crossOrigin = 'Anonymous';
-  img.onload = function () {
-    var canvas = document.createElement('CANVAS');
-    var ctx = canvas.getContext('2d');
-    var dataURL;
-    canvas.height = this.naturalHeight;
-    canvas.width = this.naturalWidth;
-    ctx.drawImage(this, 0, 0);
-    dataURL = canvas.toDataURL(outputFormat);
-    callback(dataURL);
-  };
-  img.src = src;
-  if (img.complete || img.complete === undefined) {
-    img.src =
-      'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
-    img.src = src;
-  }
-}
-
-toDataURL('./main.png', function (dataUrl) {
-  console.log('RESULT:', dataUrl);
-});
